a6e56f3a4147831a3706f01398843d2764cb8a89
adithyur <adithyur@gmail.com>
2024-06-11 21:32:06 +0530

    loader update commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react';
 import Home from "./component/Home";
 import Spinner from "./component/Spinner";
 
--- src/component/Spinner.jsx
+++ src/component/Spinner.jsx
@@ -4,11 +4,11 @@ import './styles.css'
 function Spinner() {
   return (
     <div>
-        <div className="min-h-screen flex justify-center items-center bg-black">
-            <div className="loader bg-white p-5 rounded-full flex space-x-3">
-                <div className="w-5 h-5 bg-gray-800 rounded-full animate-bounce" />
-                <div className="w-5 h-5 bg-gray-800 rounded-full animate-bounce" />
-                <div className="w-5 h-5 bg-gray-800 rounded-full animate-bounce" />
+        <div className="min-h-screen flex justify-center items-center bg-white">
+            <div className="loader bg-black p-5 rounded-full flex space-x-3">
+                <div className="w-5 h-5 bg-white rounded-full animate-bounce" />
+                <div className="w-5 h-5 bg-white rounded-full animate-bounce" />
+                <div className="w-5 h-5 bg-white rounded-full animate-bounce" />
             </div>
         </div>
     </div>
